db59aa38dff0059d0aef2ef6be2af179197c0a43
Obura Tongoi <obura@tongoi.com>
2016-02-27 17:46:54 -0800

    mobile first css

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,7 +9,7 @@
       {{-- <p><span class="type-me">Let's modernize your online presence<br> and grow your audience.</span></p> --}}
       <p><span class="type-me">I create products that thrive on the web.</span></p>
     </div>
-    <div id="bg-img" style="background: linear-gradient(-110deg, rgba(72,34,125,0.85) 0%, rgba(47,86,156,0.85) 100%), url('img/cover-8.jpeg'); background-repeat: no-repeat, no-repeat; background-position: center, center; background-size: cover;">
+    <div id="bg-img" style="background: linear-gradient(-110deg, rgba(72,34,125,0.85) 0%, rgba(47,86,156,0.85) 100%), url('img/welcome.jpeg'); background-repeat: no-repeat, no-repeat; background-position: center, center; background-size: cover;">
     </div>
   </div>
 @endsection
